0631b9e00069736fdeb83c211a397048d9592d6f
ChaseR34 <clr96@nau.edu>
2022-04-06 15:23:36 -0700

    finished the analysis for PMI0009 and PMI0010 with the three samples pruned

--- nextstrain/scripts/add_vector_from_name.py
+++ nextstrain/scripts/add_vector_from_name.py
@@ -1,2 +1,9 @@
-import pandas as pd
+import os
+
+os.chdir("/home/chase/DissertationProjects/nextstrain/WNV_Nextstrain_Analysis/")
+
+with open("nextstrain/data/metadata.tsv", "r") as meta_file:
+
+    while line := meta_file.readline():
+        print(line)
 
